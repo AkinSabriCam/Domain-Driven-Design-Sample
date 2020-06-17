89486dca0359e2674927036df72eb977884a9c1c
Akın Sabri Çam <sabricam2016@gmail.com>
2020-06-18 00:48:31 +0300

    added consts to domain.shared project and updated buttons

---
+++ Sample/Exercice.Domain.Shared/Companies/CompanyConsts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise.Domain.Shared.Companies
+{
+    public class CompanyConsts
+    {
+        public const int CompanyNameMaxLength = 256;
+        public const int HeadQuartersMaxLength = 256; 
+    }
+}
